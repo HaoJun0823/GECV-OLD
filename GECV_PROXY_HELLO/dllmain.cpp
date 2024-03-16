e772970c6e431f0f63ea9e0bc2ca613352ebed1e
HaoJun0823(Randerion) <email@haojun0823.xyz>
2024-03-16 08:09:22 +0800

    Extend Hello.

--- GECV_PROXY_HELLO/dllmain.cpp
+++ GECV_PROXY_HELLO/dllmain.cpp
@@ -3,7 +3,10 @@
 #include <windows.h>
 #include <fstream>
 #include <iostream>
+#include <filesystem>
+#include <string>
 
+namespace fs = std::filesystem;
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -16,6 +19,46 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     case DLL_PROCESS_ATTACH:
         log.open(".\\CE_DATA\\GECV_HELLO.log", std::ios::trunc | std::ios::out);
         log << "Hello Game For Process!\n";
+
+
+        if (fs::exists(".\\CE_DATA\\GECV_HELLO.bin")) {
+
+            std::ifstream str_bin(L".\\CE_DATA\\GECV_HELLO.bin", std::ios::in);
+
+            std::string message;
+
+            std::getline(str_bin, message);
+
+            std::string br = "<br>";
+            std::string replace_br = "\n";
+
+            log << "Origin Welcome:" << message << "\n";
+
+            size_t pos = 0;
+
+            while ((pos = message.find(br, pos)) != std::string::npos) {
+
+                message.replace(pos, br.length(), replace_br);
+
+            }
+            log << "Parse Welcome:" << message << "\n";
+            int size_needed = MultiByteToWideChar(CP_UTF8, 0, message.c_str(), int(message.size()), NULL, 0);
+            std::wstring wstr(size_needed, 0);
+            MultiByteToWideChar(CP_UTF8, 0, message.c_str(), int(message.size()), &wstr[0], size_needed);
+
+            
+
+            MessageBox(0,wstr.c_str(), L"GECV:Hello!", MB_OK);
+
+            str_bin.close();
+            fs::remove(".\\CE_DATA\\GECV_HELLO.bin");
+
+        }
+        else {
+            log << "No Welcome!\n";
+        }
+
+
         break;
     case DLL_THREAD_ATTACH:
         log.open(".\\CE_DATA\\GECV_HELLO.log", std::ios::app | std::ios::out);
