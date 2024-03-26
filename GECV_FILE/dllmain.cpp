1d9a0adf52017eae77581bc6d57f68be38e0613b
HaoJun0823(Randerion) <email@haojun0823.xyz>
2024-03-26 13:14:35 +0800

    Fix log system.

--- GECV_FILE/dllmain.cpp
+++ GECV_FILE/dllmain.cpp
@@ -12,7 +12,7 @@
 
 namespace fs = std::filesystem;
 
-
+static std::ofstream logg;
 static HANDLE process;
 
 static Hooker Hooker_CreateFileW;
@@ -23,6 +23,173 @@ static Hooker Hooker_PathFileExistsW;
 
 static Hooker Hooker_OutputDebugStringA;
 
+static Hooker HK0printf, HK0_vsnprintf_s, HK0_snprintf, HK0_sprintf_s, HK0_vsnprintf, HK0_swprintf_s, HK0_vscprintf, HK0sprintf, HK0vsprintf, HK0vsprintf_s, HK0wsprintfW;
+
+#pragma warning(disable : 4996)
+
+void SetPrintfStatus(BOOL status) {
+   
+        HK0printf.Set(status);
+        //HK0sprintf.Set(status);
+        HK0vsprintf.Set(status);
+        HK0vsprintf_s.Set(status);
+        HK0wsprintfW.Set(status);
+        HK0_snprintf.Set(status);
+        HK0_sprintf_s.Set(status);
+        HK0_swprintf_s.Set(status);
+        HK0_vscprintf.Set(status);
+        HK0_vsnprintf.Set(status);
+        HK0_vsnprintf_s.Set(status);
+    
+
+}
+
+int Hprintf(const char* const Format, ...) {
+    SetPrintfStatus(false);
+    va_list args, args_copy;
+    va_start(args, Format);
+    va_copy(args_copy, args);
+    char Buffer[4096];
+    int result = vsprintf(Buffer, Format, args_copy);
+    logg << "printf:" << Buffer << "\n";
+    logg.flush();
+
+
+    SetPrintfStatus(true);
+    return result;
+};
+
+int __cdecl Hvsnprintf_s(char* const Buffer, const size_t BufferCount, const size_t MaxCount, const char* const Format, va_list ArgList) {
+    SetPrintfStatus(false);
+
+
+    int ret = vsnprintf_s(Buffer, BufferCount,MaxCount, Format, ArgList);
+    logg << "vsnprintf_s:" << Buffer << "\n";
+    logg.flush();
+
+
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int Hsnprintf(char* const Buffer, const size_t BufferCount, const char* const Format, ...) {
+    va_list args, args_copy;
+    va_start(args, Format);
+    va_copy(args_copy, args);
+
+    int ret = snprintf(Buffer, BufferCount, Format, args_copy);
+    logg << "snprintf:" << Buffer << "\n";
+    logg.flush();
+    va_end(args_copy);
+
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int Hsprintf_s(char* const Buffer, const size_t BufferCount, const char* const Format, ...) {
+    SetPrintfStatus(false);
+
+    va_list args, args_copy;
+    va_start(args, Format);
+    va_copy(args_copy, args);
+
+    int ret = sprintf_s(Buffer, BufferCount, Format, args_copy);
+    logg << "sprintf_s:" << Buffer << "\n";
+    logg.flush();
+    va_end(args_copy);
+
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int __cdecl Hvsnprintf(char* const Buffer, const size_t BufferCount, const char* const Format, va_list ArgList) {
+    SetPrintfStatus(false);
+
+
+    int ret = vsnprintf(Buffer, BufferCount, Format, ArgList);
+    logg << "vsnprintf:" << Buffer << "\n";
+    logg.flush();
+
+
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int Hswprintf_s(wchar_t* const Buffer, const size_t BufferCount, const wchar_t* const Format, ...) {
+    SetPrintfStatus(false);
+    
+    va_list args, args_copy;
+    va_start(args, Format);
+    va_copy(args_copy, args);
+
+    int ret = swprintf_s(Buffer, BufferCount, Format, args_copy);
+    logg << "swprintf_s:" << Buffer << "\n";
+    logg.flush();
+    va_end(args_copy);
+
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int __cdecl Hvscprintf(const char* const Format, va_list ArgList) {
+    SetPrintfStatus(false);
+
+    int ret = _vscprintf(Format, ArgList);
+    logg << "vscprintf:" << ret << "\n";
+    logg.flush();
+
+
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int Hsprintf(char* const Buffer, const char* const Format, ...) {
+    SetPrintfStatus(false);
+    va_list args, args_copy;
+    va_start(args, Format);
+    va_copy(args_copy, args);
+    int result = sprintf(Buffer, Format, args_copy);
+    va_end(args_copy);
+    logg << "sprintf:" << Buffer;
+    logg.flush();
+    SetPrintfStatus(true);
+    return result;
+};
+
+int __cdecl Hvsprintf(char* const Buffer, const char* const Format, va_list ArgList) {
+    SetPrintfStatus(false);
+
+    int ret = sprintf(Buffer,  Format, ArgList);
+    logg << "sprintf:" << Buffer << "\n";
+    logg.flush();
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int __cdecl Hvsprintf_s(char* const Buffer, const size_t BufferCount, const char* const Format, va_list ArgList) {
+    SetPrintfStatus(false);
+
+
+    int ret = sprintf_s(Buffer,BufferCount,Format,ArgList);
+    logg << "sprintf_s:" << Buffer << "\n";
+    logg.flush();
+    SetPrintfStatus(true);
+    return ret;
+};
+
+int HwsprintfW(LPWSTR unamedParam1, LPCWSTR unamedParam2, ...) {
+    SetPrintfStatus(false);
+    va_list args, args_copy;
+    va_start(args, unamedParam2);
+    va_copy(args_copy, args);
+    int result = wsprintfW(unamedParam1, unamedParam2, va_arg(args_copy, char*));
+    va_end(args_copy);
+    logg << "wsprintfW:" << unamedParam1 << "\n";
+    logg.flush();
+    SetPrintfStatus(true);
+    return result;
+
+};
 
 HANDLE
 WINAPI
@@ -64,10 +231,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
 
-
-        fs:remove(".\\CE_DATA\\GAME_DEBUG.log");
-
         std::ofstream log;
+
+
+
+        logg.open(".\\CE_DATA\\GAME_DEBUG.log",std::ios::trunc|std::ios::out);
+
+        
         log.open(".\\CE_DATA\\GECV_FILE.log", std::ios::trunc | std::ios::out);
         log.close();
         DisableThreadLibraryCalls(hModule);
@@ -93,6 +263,21 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
         Hooker_OutputDebugStringA.Create(process, Output_Debug_A,(DWORD)Hook_OutputDebugStringA);
 
+
+        HK0printf.Create(process, "msvcr120", "printf",(DWORD)Hprintf);
+        HK0sprintf.Create(process, "msvcr120", "sprintf", (DWORD)Hsprintf);
+        HK0vsprintf.Create(process, "msvcr120", "vsprintf", (DWORD)Hvsprintf);
+        HK0vsprintf_s.Create(process, "msvcr120", "vsprintf_s", (DWORD)Hvsprintf_s);
+        HK0wsprintfW.Create(process, "user32", "wsprintfW", (DWORD)HwsprintfW);
+        HK0_snprintf.Create(process, "msvcr120", "_snprintf", (DWORD)Hsnprintf);
+        HK0_sprintf_s.Create(process, "msvcr120", "_sprintf_s", (DWORD)Hsprintf_s);
+        HK0_swprintf_s.Create(process, "msvcr120", "_swprintf_s", (DWORD)Hswprintf_s);
+        HK0_vscprintf.Create(process, "msvcr120", "_vsprintf", (DWORD)Hvsprintf);
+        HK0_vsnprintf.Create(process, "msvcr120", "_vsnprintf", (DWORD)Hvsnprintf);
+        HK0_vsnprintf_s.Create(process, "msvcr120", "_vsnprintf_s", (DWORD)Hvsnprintf_s);
+
+
+
         log << "Created Hook.\n";
 
         Hooker_CreateFileA.Install();
@@ -108,8 +293,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
         //log << "UnInstall Hook.\n";
 
-        
+       SetPrintfStatus(true);
 
+       
 
     }
 
@@ -211,12 +397,11 @@ Hook_OutputDebugStringA(
     _In_opt_ LPCSTR lpOutputString
 ){
 
-    std::ofstream log;
-    log.open(".\\CE_DATA\\GAME_DEBUG.log", std::ios::app | std::ios::out);
+    
 
-    log << "OutputDebugStringA:" << lpOutputString;
-    log.flush();
-    log.close();
+    logg << "OutputDebugStringA:" << lpOutputString;
+    logg.flush();
+    
 
 
     Hooker_OutputDebugStringA.UnInstall();
--- GECV_FILE/hooker.h
+++ GECV_FILE/hooker.h
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <fstream>
 
+
 class Hooker {
 private:
 	BYTE OLD_hook_bytes[5];
@@ -8,23 +9,9 @@ private:
 	HANDLE process;
 	FARPROC origin;
 	DWORD hook;
-public:
 
-	Hooker() {
 
-	}
-
-	void Create(HANDLE Process, FARPROC OriginAddress, DWORD HookAddress) {
-
-		//std::ofstream log;
-
-		//log.open(".\\CE_DATA\\GECV_FILE.log", std::ios::app | std::ios::out);
-
-		
-
-		process = Process;
-		origin = OriginAddress;
-		hook = HookAddress;
+	void Bind() {
 
 		DWORD memory_ret = 0;
 
@@ -39,6 +26,38 @@ public:
 		memcpy_s(&NEW_hook_bytes[1], 4, &offset, 4);
 
 		VirtualProtect(origin, 5, PAGE_EXECUTE_READWRITE, &memory_ret);
+	}
+
+public:
+
+	Hooker() {
+
+	}
+
+	void Create(HANDLE Process, LPCSTR library, LPCSTR function, DWORD HookAddress) {
+
+		process = Process;
+		
+		origin = GetProcAddress(GetModuleHandleA(library), function);
+
+		hook = HookAddress;
+
+		Bind();
+
+	}
+
+	void Create(HANDLE Process, FARPROC OriginAddress, DWORD HookAddress) {
+
+		//std::ofstream log;
+
+		//log.open(".\\CE_DATA\\GECV_FILE.log", std::ios::app | std::ios::out);
+
+		process = Process;
+		origin = OriginAddress;
+		hook = HookAddress;
+
+		Bind();
+
 		//log << "Protect:" << memory_ret << "\n";
 		//log.close();
 	}
@@ -63,6 +82,18 @@ public:
 		//log << "UnInstall:" << memory_ret << "\n";
 		//log.close();
 		return memory_ret;
+
+	}
+
+	VOID Set(BOOL status) {
+
+		if (status) {
+			Install();
+		}
+		else {
+			UnInstall();
+		}
+
 
 	}
 
