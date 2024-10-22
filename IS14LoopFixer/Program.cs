2ea1f88dd6987509cda47b57e2fda04aa50eb15f
HaoJun0823 <developer@haojun0823.xyz>
2024-10-22 21:45:46 +0800

    Just something.

---
+++ IS14LoopFixer/Program.cs
@@ -0,0 +1,169 @@
+﻿using MiniExcelLibs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS14LoopFixer
+{
+
+    struct AT9Loop
+    {
+        public int start;
+        public int end;
+    }
+
+    internal class Program
+    {
+
+
+        static Dictionary<string, AT9Loop> AT9Map = new Dictionary<string, AT9Loop>();
+
+        static void Main_Fake(string[] args)
+        {
+
+            File.Delete(AppDomain.CurrentDomain.BaseDirectory+"\\info.csv");
+
+            LoadAT9LoopInformation();
+
+
+            String[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\IS14\\", "*.is14");
+
+            Dictionary<string,string> IS14Map = new Dictionary<string,string>();
+
+            foreach(var i in files)
+            {
+                IS14Map.Add(Path.GetFileNameWithoutExtension(i), i);
+            }
+            
+
+            
+
+
+            foreach(var kv in AT9Map)
+            {
+                BuildLoopIS14(kv.Value, IS14Map[kv.Key]);
+            }
+
+
+
+           //MiniExcel.SaveAs(AppDomain.CurrentDomain.BaseDirectory + "\\info.csv",AT9Map);
+
+
+        }
+
+
+
+        static void LoadAT9LoopInformation()
+        {
+
+            String[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\AT9\\", "*.at9");
+
+
+            foreach (String file in files) {
+            
+                AT9Loop loop = new AT9Loop();
+
+                using(BinaryReader br = new BinaryReader(File.OpenRead(file)))
+                {
+
+                    br.BaseStream.Seek(0x90, SeekOrigin.Begin);
+                    loop.start = br.ReadInt32();
+                    loop.end = br.ReadInt32();
+
+                }
+            
+            
+                AT9Map.Add(Path.GetFileNameWithoutExtension(file), loop);
+            
+            }
+            
+
+
+        }
+
+
+        static void BuildLoopIS14(AT9Loop at9,string is14)
+        {
+
+            byte[] file = File.ReadAllBytes(is14);
+
+            byte[] header = file.Take(0x28).ToArray();
+
+            byte[] body = file.Skip(0x28).ToArray();
+
+
+
+            int unk1 = BitConverter.ToInt32(file,0x4) + 0x10;
+
+            int unk2 = 0x0;
+
+            byte[] unk3 = { 0X6C, 0X6F, 0X6F, 0X70, 0X0, 0X0, 0X0, 0X08 };
+
+            byte[] unk4 = BitConverter.GetBytes(at9.start).Reverse().ToArray();
+
+            byte[] unk5 = BitConverter.GetBytes(at9.end).Reverse().ToArray();
+
+            int mask = BitConverter.ToInt32(file,0x28);
+
+            if (mask == 1886351212)
+            {
+                return;
+            }
+
+
+
+            byte[] fix_header;
+
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms))
+            {
+
+                bw.Write(header);
+
+                bw.Seek(0,SeekOrigin.Begin);
+
+
+                bw.Seek(0x04,SeekOrigin.Begin);
+                bw.Write(unk1);
+                bw.Flush();
+                bw.Seek(0x10, SeekOrigin.Begin);
+                bw.Write(unk2);
+                bw.Flush();
+
+                bw.Seek(0x28, SeekOrigin.Begin);
+                bw.Write(unk3);
+                bw.Flush();
+                bw.Write(unk4);
+                bw.Flush();
+                bw.Write(unk5);
+                bw.Flush();
+
+                fix_header = ms.ToArray();
+            }
+
+
+            using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(AppDomain.CurrentDomain.BaseDirectory+"\\Build\\"+Path.GetFileNameWithoutExtension(is14)+".is14")))
+            {
+                bw.Write(fix_header);
+                bw.Write(body);
+                bw.Flush();
+            }
+
+
+
+
+
+        }
+        
+
+    }
+
+
+
+
+    
+}
---
+++ IS14LoopFixer/Program_Real.cs
@@ -0,0 +1,105 @@
+﻿using MiniExcelLibs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS14LoopFixer
+{
+    internal class Program_Real
+    {
+
+        static Dictionary<string, string> map = new Dictionary<string, string>();
+
+        static DataTable dt;
+
+        static void Main(string[] args)
+        {
+
+            string[] filespath = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory+"\\IS14\\","*.*");
+
+
+            dt = MiniExcel.QueryAsDataTable(AppDomain.CurrentDomain.BaseDirectory + "\\at9_data.xlsx", useHeaderRow: true);
+
+
+            Console.WriteLine($"You have {filespath.Length} is14 files");
+
+            foreach (string s in filespath) {
+
+
+                map.Add(Path.GetFileName(s), s);
+            
+            
+            }
+
+
+
+            foreach(DataRow r in dt.Rows)
+            {
+
+                string filename = r["Name"].ToString();
+
+
+                Console.WriteLine($"Does {filename} alive? {map.ContainsKey(filename)}");
+
+                if (map.ContainsKey(filename))
+                {
+
+                    byte[] data = File.ReadAllBytes(map[filename]);
+
+
+                    if (BitConverter.ToInt32(data,0x28) == 1886351212)
+                    {
+                        Console.WriteLine("But this is loop file!");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Make this to loop file!");
+                    }
+
+
+                    using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(AppDomain.CurrentDomain.BaseDirectory + "\\Build\\" + Path.GetFileName(map[filename]))))
+                    {
+
+                        bw.Write(data.Take(0x28).ToArray());
+
+                        bw.Seek(0x04, SeekOrigin.Begin);
+
+                        bw.Write(BitConverter.GetBytes(data.Length + 0x10).Reverse().ToArray());
+
+                        bw.Seek(0x20,SeekOrigin.Begin);
+
+                        bw.Write(0x0);
+
+                        bw.Seek(0x28, SeekOrigin.Begin);
+
+
+                        byte[] unk = { 0x6c, 0x6f, 0x6f, 0x70, 0x00, 0x00, 0x00, 0x08 };
+
+                        bw.Write(unk);
+
+                        bw.Write(Convert.ToInt32(r["Start"].ToString(),16));
+                        bw.Write(Convert.ToInt32(r["End"].ToString(),16));
+
+                        bw.Write(data.Skip(0x28).ToArray());
+
+                        bw.Flush();
+                    }
+                    
+
+
+                }
+
+            }
+            Console.WriteLine("Done");
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
